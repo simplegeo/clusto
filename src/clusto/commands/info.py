9d1572af25bea64ddb90da36249d372f49cd5e2c
Jeremy Grosser <jeremy@synack.me>
2011-08-23 21:29:33 +0000

    Make clusto-info work the way I want it to

--- src/clusto/commands/info.py
+++ src/clusto/commands/info.py
@@ -28,6 +28,11 @@ except ImportError:
     except:
         pass
 
+PROPERTY_MAP = (
+    ('Name', 'name'),
+    ('Type', 'type'),
+    ('Driver', 'driver'),
+)
 
 class Info(script_helper.Script):
     '''
@@ -38,117 +43,71 @@ class Info(script_helper.Script):
     def __init__(self):
         script_helper.Script.__init__(self)
 
-    def format_line(self, key, value, pad=20):
-        if value:
-            if isinstance(value, list):
-                value = ', '.join(value)
-            key += ':'
-            print key.ljust(pad, ' '), value
-
-    def print_summary(self, items):
-        self.debug('Printing with format=summary')
-        for item in items:
-            self.format_line('Name', item.pop('name'))
-            self.format_line('Type', item.pop('type'))
-            if 'ip' in item.keys():
-                self.format_line('IP', item.pop('ip'))
-            self.format_line('Description', item.pop('description'))
-            if 'parents' in item.keys():
-                self.format_line('Parents', item.pop('parents'))
-            if 'contents' in item.keys():
-                self.format_line('Contents', item.pop('contents'))
-            print '\n'
-            keys = sorted(item.keys())
-            for k in keys:
-                self.format_line(k.capitalize(), item[k])
-            print '-' * 80
-
-
-    def print_oneline(self, items):
-        self.debug('Printing with format=oneline')
-        for item in items:
-            line = '%s(%s);' % (item.pop('name'), item.pop('type'))
-            line += '%s;' % (','.join([ '"%s"' % _ for _ in item.pop('description') ]))
-            if 'ip' in item.keys():
-                line += 'ip=%s;' % (','.join([ _ for _ in item.pop('ip') ]))
-            if 'parents' in item.keys():
-                line += 'parents=%s;' % (','.join([ _ for _ in item.pop('parents') ]))
-            if 'contents' in item.keys():
-                line += 'contents=%s;' % (','.join([ _ for _ in item.pop('contents') ]))
-            keys = sorted(item.keys())
-            for k in keys:
-                line += '%s=%s;' % (k, item[k])
-            print line
-
-    def print_json(self, items):
-        self.debug('Printing with format=json')
-        print json.dumps(items, sort_keys=True, indent=2)
-
-    def print_yaml(self, items):
-        self.debug('Printing with format=yaml')
-        print yaml.safe_dump(items, encoding='utf-8',
-            explicit_start=True, default_flow_style=False)
-
-    def run(self, args):
-        if not args.items:
-            print 'You need to provide at least one item. Use --help'
-            return 0
-        item_list = []
-        self.debug('Fetching the list of items: %s' % ','.join(args.items))
-        for item in args.items:
-            obj = clusto.get(item)
-            if not obj:
-                self.warn("The item %s couldn't be found" % item)
-                continue
-            obj = obj[0]
-            self.debug('Object found! %s' % obj)
-            item_attrs = {
-                'name': obj.name,
-                'type': obj.type,
-            }
-#           Fetch system attrs
-            for attr in obj.attrs(key='system'):
-                item_attrs[attr.subkey] = attr.value
-#           fetch description(s)
-            values = obj.attrs(key='description')
-            if values:
-                item_attrs['description'] = [ _.value for _ in values]
-            else:
-                item_attrs['description'] = ''
-#           fetch parent(s)
-            values = obj.parents()
-            if values:
-                item_attrs['parents'] = [ _.name for _ in values ]
-#           fetch content(s)
-            values = obj.contents()
-            if values:
-                item_attrs['contents'] = [ _.name for _ in values ]
-#           fetch ip(s)
-            values = IPManager.get_ips(obj)
-            if values:
-                item_attrs['ip'] = [ _ for _ in values ]
-#           fetch mac(s)
-            values = [ _ for _ in obj.attrs(key='port-nic-eth') if _.subkey.find('mac') != -1 ]
-            if values:
-                for value in values:
-                    item_attrs['mac%d' % value.number] = value.value
-            item_list.append(item_attrs)
-        getattr(self, 'print_%s' % args.format)(item_list)
-
     def _add_arguments(self, parser):
-        choices = ['summary', 'oneline']
-        if JSON:
-            choices.append('json')
-        if YAML:
-            choices.append('yaml')
-        parser.add_argument('--format', choices=choices, default='summary',
-            help='What format to use to display the info, defaults to "summary"')
-        parser.add_argument('items', nargs='*', metavar='item',
-            help='List of one or more objects to show info')
+        parser.add_argument('items', nargs='*', metavar='item', help='List of one or more objects to show info')
+        parser.add_argument('--csv', '-c', action='store_true', help='Format output as CSV')
 
     def add_subparser(self, subparsers):
         parser = self._setup_subparser(subparsers)
         self._add_arguments(parser)
+
+    def print_line(self, key, value, pad=20):
+        if not value:
+            return None
+        if isinstance(value, list):
+            value = ', '.join(value)
+        key += ':'
+        print key.ljust(pad, ' '), value
+
+    def run(self, args):
+        for name in args.items:
+            obj = clusto.get(name)
+            if not obj:
+                sys.stderr.write('Object does not exist: %s\n' % name)
+                continue
+            obj = obj[0]
+
+            for label, key in PROPERTY_MAP:
+                value = getattr(obj, key, None)
+                self.print_line(label, value)
+
+            #sys.stdout.write('\n')
+
+            self.print_line('Parents', [x.name for x in obj.parents()])
+            self.print_line('Contents', [x.name for x in obj.contents()])
+            
+            sys.stdout.write('\n')
+
+            self.print_line('IP', x.attr_values(key='ip', subkey='ipstring'))
+            self.print_line('Public DNS', x.attr_values(key='ec2', subkey='public-dns'))
+            self.print_line('Private DNS', x.attr_values(key='ec2', subkey='private-dns'))
+
+            sys.stdout.write('\n')
+
+            self.print_line('Owner', ['%s: %s' % (x.subkey, x.value) for x in obj.attrs(key='owner')])
+            self.print_line('Serial',
+                [x.rstrip('\r\n') for x in obj.attr_values(
+                key='system', subkey='serial')])
+
+            memory = obj.attr_value(key='system', subkey='memory')
+            if memory:
+                self.print_line('Memory', '%i GB' % (int(memory) / 1000))
+
+            disk = obj.attr_value(key='system', subkey='disk')
+            if disk:
+                self.print_line('Disk', '%i GB (%i)' % (int(disk),
+                len([int(x) for x in obj.attr_values(key='disk', subkey='size')])))
+
+            cpucount = obj.attr_value(key='system', subkey='cpucount')
+            if cpucount:
+                self.print_line('CPU Cores', int(cpucount))
+
+            self.print_line('Description', obj.attr_values(key='description'))
+            sys.stdout.write('\n')
+
+            if len(args.items) > 1:
+                sys.stdout.write(('-' * 40) + '\n\n')
+
 
 def main():
     info, args = script_helper.init_arguments(Info)
